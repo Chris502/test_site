28b090f9faba89646562661f1dc97906d3c8e822
chris502 <canderson0289@gmail.com>
2016-10-18 16:50:56 -0400

    Added javascript for username ,and picture changing

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,30 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/test_image.jpg') {
+      myImage.setAttribute ('src','images/test_image2.jpg');
+    } else {
+      myImage.setAttribute ('src','images/test_image.jpg');
+    }
+}
+// setting up a clicable button to input user name
+var myButton = document.querySelector('button');
 var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello World!';
+
+function setUserName() {
+	var myName = prompt('Please enter your name.');
+	localStorage.setItem('name', myName);
+	myHeading.textContent = 'Coding is Awesome, ' + myName;
+}
+//setting up to check if we have name if not prompts for Name
+if(!localStorage.getItem('name')) {
+	setUserName();
+} 	else {
+	var storedName = localStorage.getItem('name');
+	myHeading.textContent = 'Coding is awesome, ' +storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
